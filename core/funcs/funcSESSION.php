e90c8ff16c9a2b504cff4c0b9cd2ead697e34df9
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2013-12-14 11:46:30 +0100

    eee

--- core/funcs/funcSESSION.php
+++ core/funcs/funcSESSION.php
@@ -22,10 +22,8 @@ function getRealIpAddr()
 
 
 function getUniqueCode($length){
-		
 $code = md5(uniqid(rand(), true));
-if ($length != "") return substr($code, 0, $length);
-    else return $code;
+return $code;
 }
 
 
