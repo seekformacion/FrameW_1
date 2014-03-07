294f295b857c1e1b785e871035ef4710ff647718
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-03-07 17:31:56 +0100

    ss

--- core/funcs/funcSESSION.php
+++ core/funcs/funcSESSION.php
@@ -1,16 +1,65 @@
 <?php
 
 
+function getDatSKUID($skpUID){global $idSES;
+$datos=json_decode(decryptIt($skpUID), TRUE);	
+
+if(is_array($datos)){
+if(array_key_exists('idSES', $datos)){
+$chkSES=$datos['idSES'];
+if($chkSES==$idSES){
+	
+	$cents="";
+	foreach ($datos['idcs'] as $key => $idc) {
+	$cents.=$idc . ",";	
+	}	
+	$cents=substr($cents,0,-1); $firstC=$datos['idcs'][0];
+
+if (isset($_COOKIE["selC"])){$datos['cent_sel']=$_COOKIE["selC"];}else{$datos['cent_sel']=$firstC;};
+$datos['lcents']=$cents;
+
+
+return $datos;	
+}
+
+}}
+
+
+	
+return FALSE;	 
+}
+
+
+
+function ascii2hex($ascii) {
+$hex = '';
+for ($i = 0; $i < strlen($ascii); $i++) {
+$byte = strtoupper(dechex(ord($ascii{$i})));
+$byte = str_repeat('0', 2 - strlen($byte)).$byte;
+$hex.=$byte." ";
+}
+return $hex;
+}
+
+function hex2ascii($hex){
+$ascii='';
+$hex=str_replace(" ", "", $hex);
+for($i=0; $i<strlen($hex); $i=$i+2) {
+$ascii.=chr(hexdec(substr($hex, $i, 2)));
+}
+return($ascii);
+}
+
 
 function encryptIt($string) {global $cryptKey;
-$output=urlencode((mcrypt_encrypt(MCRYPT_RIJNDAEL_256, md5($cryptKey), $string, MCRYPT_MODE_CBC, md5(md5($cryptKey)))));
-$output = str_replace("+", "%2B",$output);
-$output = str_replace(".", "|",$output);
+$output=ascii2hex((mcrypt_encrypt(MCRYPT_RIJNDAEL_256, md5($cryptKey), $string, MCRYPT_MODE_CBC, md5(md5($cryptKey)))));
+//$output = str_replace("+", "%2B",$output);
+//$output = str_replace(".", "|",$output);
 return $output;
 }
 
 function decryptIt($string) {global $cryptKey;
-$string = str_replace("|", ".",$string);
+$string = hex2ascii($string);
 $output = rtrim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, md5($cryptKey), $string, MCRYPT_MODE_CBC, md5(md5($cryptKey))), "\0");
 return $output;
 }
