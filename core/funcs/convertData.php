986b69c5836d35f730b0ee801435f6353ed78639
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-05-14 14:45:51 +0200

    eee

--- core/funcs/convertData.php
+++ core/funcs/convertData.php
@@ -15,11 +15,24 @@ return $ncamp;
 function campforPanel($cmp,$val,$idc){
 $nval=$val;
 
+#####campo fecha de nacimiento
+if($cmp==12){
+
+
+$res= DBselectSDB("SELECT panel FROM skP_equivals WHERE id_centro=0 AND id_campo=$cmp;",'seekpanel'); 
+if(array_key_exists(1, $res)){$nval=$res[1]['panel'];};
+$res= DBselectSDB("SELECT panel FROM skP_equivals WHERE id_centro=$idc AND id_campo=$cmp;",'seekpanel'); 
+if(array_key_exists(1, $res)){$nval=$res[1]['panel'];};		
+$nval=str_replace('aaaa',substr($val,0,4),$nval);		
+$nval=str_replace('mm',substr($val,4,2),$nval);	
+$nval=str_replace('dd',substr($val,6,2),$nval);		
+
+}else{
 $res= DBselectSDB("SELECT panel FROM skP_equivals WHERE id_centro=0 AND id_campo=$cmp AND cod='$val';",'seekpanel'); 
 if(array_key_exists(1, $res)){$nval=$res[1]['panel'];};
 $res= DBselectSDB("SELECT panel FROM skP_equivals WHERE id_centro=$idc AND id_campo=$cmp AND cod='$val';",'seekpanel'); 
 if(array_key_exists(1, $res)){$nval=$res[1]['panel'];};
-
+}
 
 return $nval;	
 }
